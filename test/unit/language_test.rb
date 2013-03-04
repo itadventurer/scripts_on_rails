76b2609389256f4f639f50ffb354cd4375dd87f6
Anatolij Zelenin <az@azapps.de>
2013-03-04 11:22:07 +0100

    Annotations

--- test/unit/language_test.rb
+++ test/unit/language_test.rb
@@ -7,6 +7,7 @@
 #  editor_config :text
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
+#  bin           :string(255)
 #
 
 require 'test_helper'
--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -5,7 +5,6 @@
 #  id          :integer          not null, primary key
 #  name        :string(255)
 #  description :string(255)
-#  cli         :string(255)
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  language_id :integer
